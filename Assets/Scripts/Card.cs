3d36445266e235830432290253197d426774d338
coderduan <duanjingyao2011@163.com>
2017-04-04 23:20:29 +0800

    Added some basic classes, but not completely implemented yet.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CardType
+{
+    ATTACK = 0,
+    DEFENCE,
+    TACTICAL,
+    SPELL,
+    MANA,
+};
+
+public class Card {
+
+    private string name;
+    private CardType cardType;
+
+    public Card(string name)
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Creature {
+
+    public string name;
+    public int health;
+    public int mana;
+    public int action;
+
+    private List<Card> deck;
+
+    // Use this for initialization
+    Creature (string nameString, int hp, int mp, int ap) {
+        name = nameString;
+        health = hp;
+        mana = mp;
+        action = ap;
+	}
+}
